d12233037556a9b66fcd17d3af8450446158dcb6
Giuseppe <giuseppe.buccellato@web.de>
2020-03-24 20:09:51 +0100

    insert, heap, quick working/merge sort missing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,40 +5,15 @@ import java.io.FileNotFoundException;
 
 public class Main {
 
-
-<<<<<<< HEAD
-    public static void main(String[] args) throws FileNotFoundException{
-
-        long timeStart;
-        long timeEnd;
-        UtilityTool util = new UtilityTool();
-        Scanner input = new Scanner(System.in);
-
-	    System.out.println("Bitte den ABSOLUTEN Pfad der zu sortierenden Datei angeben. " +
-                "(Z.B. C:\\Users\\ericw\\IdeaProjects\\Sorting Algorithms\\res\\daten_100k)");
-
-
-	    String path = input.nextLine();
-
-	    File file = new File(path);
-
-        Sorter sorter = new Sorter();
-
-        int elements = util.getLengthOfFile(file);
-
-        int[] array = util.toArray(file, elements);
-
-	    System.out.println("\n Sortierverfahren auswaehlen: \n Insertion-Sort (1) \n Heap-Sort \t\t(2)\n Quick-Sort \t(3)\n Merge-Sort \t(4)");
-	    int select = input.nextInt();
-        switch(select){
-=======
 public static void main(String[] args) throws FileNotFoundException {
+
     long timeStart;
     long timeEnd;
     UtilityTool util = new UtilityTool();
     Scanner input = new Scanner(System.in);
 
-    System.out.println("State ABSOLUTE path to file (e.g.: C:\\Users\\ericw\\IdeaProjects\\Sorting Algorithms\\res\\daten_100k)");
+    System.out.println("State ABSOLUTE path to file " +
+            "(e.g.: C:\\Users\\ericw\\IdeaProjects\\Sorting Algorithms\\res\\daten_100k)");
 
     String path = input.nextLine();
     File file = new File(path);
@@ -46,10 +21,10 @@ public static void main(String[] args) throws FileNotFoundException {
     int elements = util.getLengthOfFile(file);
     int[] array = util.toArray(file, elements);
 
-    System.out.println("\n Choose sorting algorithm: \n Insertionsort (1) \n Heapsort \t\t(2)\n Quicksort \t(3)\n Mergesort \t(4)");
+    System.out.println("\n Choose sorting algorithm: " +
+            "\n Insertionsort (1) \n Heapsort \t\t(2)\n Quicksort \t(3)\n Mergesort \t(4)");
         int select = input.nextInt();
         switch (select) {
->>>>>>> eb2e761466643b95ce793d66438219524c1ee81d
             case 1:
                 timeStart = System.currentTimeMillis();
                 sorter.insertion(array);
