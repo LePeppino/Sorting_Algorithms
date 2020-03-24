0da33059161e8ec99aaf605cdabaec8db983e9fd
EricWalter5174 <62294021+EricWalter5174@users.noreply.github.com>
2020-03-24 17:13:39 +0100

    Insertion-, Quick- and Heapsort implemented, fully functional. Mergesort waiting to be implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,56 +6,54 @@ import java.io.FileNotFoundException;
 public class Main {
 
 
-    public static void main(String[] args) throws FileNotFoundException{
+public static void main(String[] args) throws FileNotFoundException {
+    long timeStart;
+    long timeEnd;
+    UtilityTool util = new UtilityTool();
+    Scanner input = new Scanner(System.in);
 
-        long timeStart;
-        long timeEnd;
-        UtilityTool util = new UtilityTool();
-        Scanner input = new Scanner(System.in);
+    System.out.println("State ABSOLUTE path to file (e.g.: C:\\Users\\ericw\\IdeaProjects\\Sorting Algorithms\\res\\daten_100k)");
 
-	    System.out.println("Bitte den ABSOLUTEN Pfad der zu sortierenden Datei angeben. (Z.B. C:\\Users\\ericw\\IdeaProjects\\Sorting Algorithms\\res\\daten_100k)");
+    String path = input.nextLine();
+    File file = new File(path);
+    Sorter sorter = new Sorter();
+    int elements = util.getLengthOfFile(file);
+    int[] array = util.toArray(file, elements);
 
-
-	    String path = input.nextLine();
-
-	    File file = new File(path);
-
-        Sorter sorter = new Sorter();
-
-        int elements = util.getLengthOfFile(file);
-
-        int[] array = util.toArray(file, elements);
-
-	    System.out.println("\n Sortierverfahren auswaehlen: \n Insertion-Sort (1) \n Heap-Sort \t\t(2)\n Quick-Sort \t(3)\n Merge-Sort \t(4)");
-	    int select = input.nextInt();
-        switch(select){
+    System.out.println("\n Choose sorting algorithm: \n Insertionsort (1) \n Heapsort \t\t(2)\n Quicksort \t(3)\n Mergesort \t(4)");
+        int select = input.nextInt();
+        switch (select) {
             case 1:
                 timeStart = System.currentTimeMillis();
                 sorter.insertion(array);
                 timeEnd = System.currentTimeMillis();
-                System.out.println("Dauer des Insertion-Sort bei " + elements + " Elementen: " +  (timeEnd - timeStart) + "ms");
+                util.checkIfSorted(array);
+                System.out.println("Time to sort array of " + elements + " Elements using Insertionsort: " + (timeEnd - timeStart) + "ms");
                 break;
             case 2:
                 timeStart = System.currentTimeMillis();
                 sorter.heap(array);
                 timeEnd = System.currentTimeMillis();
-                System.out.println("Dauer des Heap-Sort bei " + elements + " Elementen: " +  (timeEnd - timeStart) + "ms");
+                util.checkIfSorted(array);
+                System.out.println("Time to sort array of " + elements + " Elements using Heapsort: " + (timeEnd - timeStart) + "ms");
                 break;
             case 3:
                 timeStart = System.currentTimeMillis();
                 sorter.quick(array, 0, array.length-1);
                 timeEnd = System.currentTimeMillis();
-                System.out.println("Dauer des Quicksort bei " + elements + " Elementen: " +  (timeEnd - timeStart) + "ms");
+                util.checkIfSorted(array);
+                System.out.println("Time to sort array of " + elements + " Elements using Quicksort: " + (timeEnd - timeStart) + "ms");
+                break;
             case 4:
                 timeStart = System.currentTimeMillis();
                 sorter.merge(array);
                 timeEnd = System.currentTimeMillis();
-                System.out.println("Dauer des Merge-Sort bei " + elements + " Elementen: " +  (timeEnd - timeStart) + "ms");
-
+                util.checkIfSorted(array);
+                System.out.println("Time to sort array of " + elements + " Elements using Mergesort: " + (timeEnd - timeStart) + "ms");
         }
 
-        util.checkIfSorted(array);
 
     }
 
 }
+
